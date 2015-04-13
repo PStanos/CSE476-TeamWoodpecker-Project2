cd479685de30f70467dcedeb96c67a5537157f57
akambeitz152 <akambeitz152@gmail.com>
2015-04-13 12:06:51 -0400

    Remember Option
    Added a remember checkbox to opening activity. During OnCreate, the app
    checks for a local text file, and either grabs a string from it or
    creates an empty version of it. The string is split on a '&' character
    and the username and password fields are filled with the result.
    During login, if the checkbox is checked, the app will grab the values
    from the username and password fields and write them into the text file.
    Very little error checking or special case handling is done at this
    point.

--- app/src/main/java/edu/msu/stanospa/teamwoodpecker_project2/MainActivity.java
+++ app/src/main/java/edu/msu/stanospa/teamwoodpecker_project2/MainActivity.java
@@ -1,27 +1,98 @@
 package edu.msu.stanospa.teamwoodpecker_project2;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends ActionBarActivity {
 
     private Game game;
+    private File FileRemember;
 
+    private EditText username;
+    private EditText password;
+    private CheckBox remember;
+
+    private static final String FILE_REMEMBER = "REMEMBER_ID_PASSWORD";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         game = new Game(this);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        remember = (CheckBox)findViewById(R.id.checkRemember);
+
+
+        try {
+            FileInputStream rememberIn = openFileInput(FILE_REMEMBER);
+
+            StringBuffer rememberInRead = new StringBuffer();
+            String rememberString = "";
+
+            InputStreamReader rememberInStream = new InputStreamReader(rememberIn);
+            BufferedReader rememberInBuff = new BufferedReader(rememberInStream);
+            while ((rememberString = rememberInBuff.readLine()) != null) {
+                rememberInRead.append(rememberString);
+            }
+            rememberInBuff.close();
+            String Remember_This = rememberInRead.toString();
+
+            if(Remember_This != "") {
+                String[] Remember_These = Remember_This.split("&", 2);
+                if(Remember_These.length == 2) {
+                    EditText username = (EditText) findViewById(R.id.username);
+                    username.setText(Remember_These[0]);
+                    EditText password = (EditText) findViewById(R.id.password);
+                    password.setText(Remember_These[1]);
+                }
+            }
+
+        }catch(IOException FileNotFoundException){
+            // file not found, does not exist, no user data stored locally
+            try {
+                String data="";
+                OutputStreamWriter rememberCreate = new OutputStreamWriter(openFileOutput(FILE_REMEMBER, Context.MODE_PRIVATE));
+                rememberCreate.write(data);
+                rememberCreate.close();
+            }
+            catch (IOException e) {
+                // fail silently
+            }
+        }
     }
 
     public void onLogIn(final View view) {
         final String username = ((EditText)findViewById(R.id.username)).getText().toString();
         final String password = ((EditText)findViewById(R.id.password)).getText().toString();
+
+        if(remember.isChecked()){
+            String rememberInfo = username.toString()+'&'+password.toString();
+            try {
+                FileOutputStream outputStream = openFileOutput(FILE_REMEMBER, Context.MODE_PRIVATE);
+                outputStream.write(rememberInfo.getBytes());
+                outputStream.close();
+            } catch (Exception IOException) {
+                //fail silently on remember data
+            }
+            remember.setChecked(false);
+        }
 
         new Thread(new Runnable() {
             @Override
